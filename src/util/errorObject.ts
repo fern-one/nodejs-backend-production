ab9560354016415a89693fea03668e9957ffe7e3
fern-one <fernwanwisa12105@gmail.com>
2024-12-30 17:53:42 +0700

    feat: Logger & Source Map support

--- src/util/errorObject.ts
+++ src/util/errorObject.ts
@@ -3,6 +3,7 @@ import responseMessage from '../constant/responseMessage'
 import { THttpError } from '../types/types'
 import config from '../config/config'
 import { EApplicationEnvironment } from '../constant/application'
+import logger from './logger'
 
 // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 export default (err: Error | unknown, req: Request, errorStatusCode: number = 500): THttpError => {
@@ -20,8 +21,7 @@ export default (err: Error | unknown, req: Request, errorStatusCode: number = 50
     }
 
     // Log
-    // eslint-disable-next-line no-console
-    console.error(`CONTROLLER_RESPONSE`, {
+    logger.error(`CONTROLLER_RESPONSE`, {
         meta: errorObj
     })
 
