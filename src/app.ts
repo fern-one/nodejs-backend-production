aaaad6a0d641249877d88b7e5c2884b450bee043
fern-one <fernwanwisa12105@gmail.com>
2025-01-08 16:39:56 +0700

    feat: Cors middleware setup

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,19 @@ import router from './router/apiRouter'
 import globalErrorHandler from './middleware/globalErrorHandler'
 import responseMessage from './constant/responseMessage'
 import httpError from './util/httpError'
+import helmet from 'helmet'
+import cors from 'cors'
 
 const app: Application = express()
 
 // Middleware
+app.use(helmet())
+app.use(
+    cors({
+        methods:['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+        origin: ['https://client.com'],
+        credentials: true
+}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
